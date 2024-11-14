f4365fdd83a880205a279ff72664707ae609801e
Tanmay Rathore <122898414+Tanmay42224@users.noreply.github.com>
2024-11-14 11:48:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "24449391"))
 API_HASH = environ.get("API_HASH", "4e7e2777f84b38a483b103f8e605a891")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7432911680:AAG05Gx7E8_dgDZpiUSpytevlcYKrWGbSaQ")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2090263808').split()]
